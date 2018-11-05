6bd1965d07878f4803cf6191404b8170ca51f618
Alexander Inman <fujiwara06@gmail.com>
2018-11-04 23:25:43 -0500

    Initial commit.

---
+++ Lab11/MovieMaker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Lab11
+{
+    class MovieMaker
+    {
+        public static List<Movie> Create()
+        {
+            var result = new List<Movie>();
+
+            result.Add(new Movie("Star Wars", "scifi"));
+            result.Add(new Movie("Star Trek", "scifi"));
+            result.Add(new Movie("Avatar", "scifi"));
+            result.Add(new Movie("Flash Gordon", "scifi"));
+            result.Add(new Movie("Battlestar Galactica", "scifi"));
+            result.Add(new Movie("Obi Wan feat Ewan McGregor", "scifi"));
+
+            result.Add(new Movie("Friday the 13th", "horror"));
+            result.Add(new Movie("Friday the 13th", "horror"));
+            result.Add(new Movie("Friday the 13th", "horror"));
+            result.Add(new Movie("Friday the 13th", "horror"));
+            result.Add(new Movie("Friday the 13th", "horror"));
+            result.Add(new Movie("Friday the 13th", "horror"));
+
+
+            return result;
+        }
+    }
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Movie List Application!");
+            Console.WriteLine();
+            Movie movie = new 
+
+            Console.ReadKey();
+        }
+        //public static int GetNumber()
+        //{
+        //    int number;
+        //    bool parsed;
+        //    do
+        //    {
+        //        var answer = Console.ReadLine();
+        //        parsed = int.TryParse(answer, out number);
+        //    } while (parsed == false);
+
+        //    return number;
+        //}
+    }
+
+    class Movie
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public Movie(string title, string category)
+        {
+            Title = title;
+            Category = category;
+            var movies = MovieMaker.Create();
+            foreach (var item in movies)
+                Console.WriteLine(item.Title);
+        }
+        enum Categories { animated = 1, drama = 2, horror = 3, scifi = 4 }
+        
+    }
+}
